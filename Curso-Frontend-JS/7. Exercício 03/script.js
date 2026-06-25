b463f1460accc8f197e9e09431db830e68d7c3f3
RafaelR2O <rafaelr.osteam@gmail.com>
2026-06-25 10:55:03 -0300

    adiciona novo script de conversão de medidas e atualiza o script existente com fórmulas detalhadas

--- Curso-Frontend-JS/7. Exercício 03/script.js
+++ Curso-Frontend-JS/7. Exercício 03/script.js
@@ -2,7 +2,19 @@ const entrada = window.prompt("Digite o valor em metros (m) que deseja converter
 const valorMetros = parseFloat(entrada)
 
 const opcao = parseFloat(window.prompt("Selecione a opção que deseja converter: \n1. Milímetro (mm) \n2. Centímetro (cm) \n3. Decímetro (dm) \n4. Decâmetro (dam) \n5. Hectômetro (hm) \n6. Quilômetro (km")
-)
+
+
+/* 
+Conversão de metros (m)	Fórmula
+
+Metros → Milímetros (mm)	mm = m × 1000
+Metros → Centímetros (cm)	cm = m × 100
+Metros → Decímetros (dm)	dm = m × 10
+Metros → Decâmetros (dam)	dam = m ÷ 10
+Metros → Hectômetros (hm)	hm = m ÷ 100
+Metros → Quilômetros (km)	km = m ÷ 1000
+
+*/)
 
 let calculo = 0
 
@@ -36,17 +48,3 @@ switch (opcao) {
         break;
 }
 
-
-
-
-/* 
-Conversão de metros (m)	Fórmula
-
-Metros → Milímetros (mm)	mm = m × 1000
-Metros → Centímetros (cm)	cm = m × 100
-Metros → Decímetros (dm)	dm = m × 10
-Metros → Decâmetros (dam)	dam = m ÷ 10
-Metros → Hectômetros (hm)	hm = m ÷ 100
-Metros → Quilômetros (km)	km = m ÷ 1000
-
-*/
